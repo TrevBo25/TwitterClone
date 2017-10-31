977e77312930ac291bac8c33fe5861f661a9a563
Trevbo25 <trevorbrown25@gmail.com>
2017-10-31 09:46:35 -0700

    started bcrypt

--- server/server.js
+++ server/server.js
@@ -1,10 +1,15 @@
 const express = require('express'),
 bodyParser = require('body-parser')
-controller = require('./controller');
+controller = require('./controller')
+bcrypt = require('bcrypt');
 
 const app = express();
 app.use(bodyParser.json());
+const saltRounds = 5;
 
 
-const PORT = 3838;
+
+
+
+const PORT = 8008;
 app.listen(PORT, () => console.log('listening on port: ', PORT));
