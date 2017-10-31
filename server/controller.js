7eaf1f7fc67e419ed82b2c6a985ad6ff6e95a77e
Trevbo25 <trevorbrown25@gmail.com>
2017-10-31 13:52:40 -0700

    started the controller

--- server/controller.js
+++ server/controller.js
@@ -1,3 +1,60 @@
+
 module.exports = {
-    
+
+    registerUser(req, res){
+        const db = req.app.get('db');
+        const {fname, lname, handle, email, password} = req.body;
+        db.check_handle([handle])
+        .then(response => {
+            if(response.length != 0){
+                res.status(409).send('Handle already exists')
+            } else {
+                db.check_email([email])
+                .then(response => {
+                    if(response.length != 0){
+                        res.status(409).send('Email already exists')
+                    } else {
+                        db.register_user([name, handle, email])
+                        .then( response => {
+                            res.status(200).send(response.data);
+                        }).catch( err => console.log('register_user', err));
+                    }
+                }).catch(err => console.log('check_email', err));
+            }
+        }).catch(err => console.log('check_handle', err));
+    },
+    login(req, res){
+        const db = req.app.get('db');
+        const {password} = req.body;
+        let login = '';
+        if (req.body.handle){
+            login = req.body.handle;
+            db.login_handle([login, password])
+            .then(response => {
+                if(response.length != 0){
+                    res.status(404).send('User does not exists');
+                } else {
+                    res.status(200).send(response.data);
+                }
+            }).catch( err => console.log('login_password', err))
+        } else {
+            login = req.body.email;
+            db.login_email([login, password])
+            .then(response => {
+                if(response.length != 0){
+                    res.status(404).send('User does not exists');
+                } else {
+                    res.status(200).send(response.data);
+                }
+            }).catch( err => console.log('login_email', err))
+        }
+    },
+    logout(req, res){
+        //kill the state in redux
+    },
+    updateUser(req, res){
+        const db = req.app.get('db');
+        const {id} = req.body
+    }
+
 }
--- server/server.js
+++ server/server.js
@@ -1,14 +1,22 @@
+
+require('dotenv').config();
 const express = require('express'),
-bodyParser = require('body-parser')
-controller = require('./controller')
-bcrypt = require('bcrypt');
+      bodyParser = require('body-parser'),
+      controller = require('./controller'),
+      bcrypt = require('bcrypt'),
+      massive = require('massive'),
+      session = require('express-session'),
+      axios = require('axios'),
+      cors = require('cors');
 
 const app = express();
+app.use(cors());
 app.use(bodyParser.json());
 const saltRounds = 5;
 
-
-
+massive(process.env.CONNECTION_STRING).then( db => {
+    app.set('db', db);
+}).catch('err', err => console.log(err))
 
 
 const PORT = 8008;
