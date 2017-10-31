ff889fab8dbf93e044ebc7ba7a00d669a127d101
Austin Triggs <triggs_austin@yahoo.com>
2017-10-31 14:04:16 -0700

    edit_user.sql updated

--- db/edit_user.sql
+++ db/edit_user.sql
@@ -2,9 +2,8 @@ UPDATE USERS
 SET NAME = $1,
     HANDLE = $2,
     EMAIL = $3,
-    PASSWORD = $4,
-    AVATAR = $5,
-    BIO = $6,
-    LOCATION = $7,
-    COVER = $8,
-WHERE ID = $9
+    AVATAR = $4,
+    BIO = $5,
+    LOCATION = $6,
+    COVER = $7,
+WHERE ID = $8
