b83d31cd5431b27dd11895ff845b6c140c19773b
IanGirard <iankeinert@gmail.com>
2017-10-31 09:42:13 -0700

    added sass

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import '../src/styles/main.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
