2837a2c50542faa22d83b8bbf1f987a774fda695
IanGirard <iankeinert@gmail.com>
2017-11-03 12:34:07 -0700

    another settings commit

--- src/components/Settings/Settings.js
+++ src/components/Settings/Settings.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
-class Settings extends Component {
+export default class Settings extends Component {
     constructor(props) {
         super(props);
         this.state = {
@@ -54,6 +54,7 @@ class Settings extends Component {
     
       render() {
         return (
+          
           <form onSubmit={this.handleSubmit}>
             <label>
               New Username:
