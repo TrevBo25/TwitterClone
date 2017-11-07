e7acfc898d0e6064cc8c10572bca642e92c0f1e3
Trevbo25 <trevorbrown25@gmail.com>
2017-11-06 17:42:32 -0700

    made things look pretty

--- src/components/Widgets/Identity/IdentityCard.js
+++ src/components/Widgets/Identity/IdentityCard.js
@@ -14,32 +14,29 @@ class IdentityCard extends Component {
                 <div className="cover">
                     <img src={this.props.pageData.userData.cover} />
                 </div>
-                <div>
-                    <div className="avatar">
-                        <img src={this.props.pageData.userData.avatar} />
-                        <div className="line">
-                            <span id="name">{this.props.pageData.userData.name}</span>
-                            <span>  {'@' + this.props.pageData.userData.handle}</span>
-                        </div>
+                <div className="avatar">
+                    <img src={this.props.pageData.userData.avatar} />
+                    <div className="line">
+                        <span id="name">{this.props.pageData.userData.name}</span>
+                        <span id="handle">  {'@' + this.props.pageData.userData.handle}</span>
                     </div>
-                    <div className="sime">
+                </div>
+                <div className="sime">
 
-                        <div id="cosd">
-                            <span>Followers</span>
-                            <span>{this.props.pageData.followers.length}</span>
-                        </div >
+                    <div className="numberlabel">
+                        <h1 className="actuallabel">Followers</h1>
+                        <h1 className="number">{this.props.pageData.followers.length}</h1>
+                    </div >
 
-                        <div id="cosd">
-                            <span>Following</span>
-                            <span>{this.props.pageData.following.length}</span>
-                        </div>
-
-                        <div id="cosd">
-                            <span>Posts</span>
-                        </div >
-
-
+                    <div className="numberlabel">
+                        <h1 className="actuallabel">Following</h1>
+                        <h1 className="number">{this.props.pageData.following.length}</h1>
                     </div>
+
+                    <div className="numberlabel">
+                        <h1 className="actuallabel">Posts</h1>
+                        <h1 className="number">0</h1>
+                    </div >
                 </div>
                 {/* {this.props.pageData.name}
                 {this.props.pageData.handle}
@@ -47,7 +44,7 @@ class IdentityCard extends Component {
                 <img src={this.props.pageData.avatar} />
                 {this.props.pageData.bio}
                 {this.props.pageData.location} */}
-            </div>
+                </div>
                     </div>
                     : <div></div>
                 }
