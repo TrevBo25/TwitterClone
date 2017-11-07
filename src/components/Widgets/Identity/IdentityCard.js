7356ec982643cba1b78ce91a537eded0fa406163
Trevbo25 <trevorbrown25@gmail.com>
2017-11-06 18:13:10 -0700

    fixed the pushed up merge conflicts...

--- src/components/Widgets/Identity/IdentityCard.js
+++ src/components/Widgets/Identity/IdentityCard.js
@@ -20,27 +20,8 @@ class IdentityCard extends Component {
                         <span id="name">{this.props.pageData.userData.name}</span>
                         <span id="handle">  {'@' + this.props.pageData.userData.handle}</span>
                     </div>
-<<<<<<< HEAD
-                    <div className="sime">
-
-                        <div id="cosd">
-                            <span>Followers</span>
-                            <span>{this.props.pageData.followers.length}</span>
-                        </div >
-
-                        <div id="cosd">
-                            <span>Following</span>
-                            <span>{this.props.pageData.following.length}</span>
-                        </div>
-
-                        <div id="cosd">
-                            <span>Posts</span>
-                            <span>{this.props.pageData.posts.length}</span>
-                        </div >
-=======
                 </div>
                 <div className="sime">
->>>>>>> 7f54d604c6a5d6b059dc61be13136718d5cdb0f4
 
                     <div className="numberlabel">
                         <h1 className="actuallabel">Followers</h1>
@@ -57,9 +38,6 @@ class IdentityCard extends Component {
                         <h1 className="number">0</h1>
                     </div >
                 </div>
-<<<<<<< HEAD
-            </div>
-=======
                 {/* {this.props.pageData.name}
                 {this.props.pageData.handle}
                 <img src={this.props.pageData.cover} />
@@ -67,7 +45,6 @@ class IdentityCard extends Component {
                 {this.props.pageData.bio}
                 {this.props.pageData.location} */}
                 </div>
->>>>>>> 7f54d604c6a5d6b059dc61be13136718d5cdb0f4
                     </div>
                     : <div>{this.props.user.handle}</div>
                 }
