8ff8737829a8fa1d7ac3fafea060bcd72d7a5a78
IanGirard <iankeinert@gmail.com>
2017-11-10 10:46:37 -0700

    talko stuff

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -3,10 +3,10 @@ import {connect} from 'react-redux';
 import { Link } from 'react-router-dom';
 import {updateUser} from './../../ducks/reducer'
 import logo from '../../assets/tacoLogo/taco.svg';
-import { Link } from 'react-router-dom';
 import { goToSettings, viewProfile } from '../../ducks/reducer';
 
 class Nav extends Component {
+
 
     goToSettings(bool){
         this.props.goToSettings(bool);
@@ -23,16 +23,15 @@ class Nav extends Component {
                     <div className="nav-logo"><Link to="" ><img src={logo}/></Link></div>
 
                     <div className="talko-box-container">
-                        <textarea className="talko-box" rows="1" cols="30" wrap="hard" maxlength="80" type="text" placeholder="Let's Talko Bout It"/>
+                        <textarea className="talko-box" rows="1" cols="30" wrap="hard" maxLength="80" type="text" placeholder="Let's Talko Bout It"/>
                     </div>
-
                     
-                    <div className="dropdown"><img src={this.props.user.avatar} onClick={() => this.goToSettings(false)}/>
+                    <div className="dropdown"><img src={this.props.user.userData.avatar} onClick={() => this.goToSettings(false)}/>
                     <div className="dropdown-container">
                       <ul className="dropdown-content">
                         <li className="user-info">
-                            <a className="usernname" href={`/#/${this.props.user.handle}`}>{this.props.user.name}</a><br/>
-                            <a href={`/#/${this.props.user.handle}`}>@{this.props.user.handle}</a>
+                            <a className="usernname" href={`/#/${this.props.user.userData.handle}`}>{this.props.user.userData.name}</a><br/>
+                            <a href={`/#/${this.props.user.userData.handle}`}>@{this.props.user.userData.handle}</a>
                         </li>
                         <li onClick={() => this.goToSettings(true)}><Link to="" >Settings</Link></li>
                         <li><a href="#">Logout</a></li>
